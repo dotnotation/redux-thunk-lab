6b73c90c55fd8fe8be1e64bb55c225f89efb1a67
dotnotation <dotinmotionproductions@gmail.com>
2022-02-25 19:09:52 -0500

    commit tree e6994acdd6d7c1eceb2f7ca307a52d68967d1656

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,18 @@
-// write your CatList component here
+import React, { Component } from 'react'
+
+export class CatList extends Component {
+    renderCats = () => {
+        return this.props.catPics.map(cp => <img key={cp.id} src={cp.url} alt="Cat" width="200" height="200" />)
+    }
+  
+    render() {
+    return (
+      <div>
+        {this.renderCats()}
+      </div>
+      
+    )
+  }
+}
+
+export default CatList
